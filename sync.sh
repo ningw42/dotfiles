31749edebf1f5a3e86f4b9d5c3a49eea2f9d61a0
Ning Wang <unidentifiedme26@gmail.com>
2020-09-20 14:09:19 +0800

    add sync.sh

---
+++ sync.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+cp ~/.zshrc .
+cp ~/.tmux.conf .
+cp ~/.spaceship.zsh .
+cp ~/.p10k.zsh .
+cp ~/.ideavimrc .
+
+# inside ~/.config
+cp ~/.config/alacritty/alacritty.yml .config/alacritty/alacritty.yml
+cp ~/.config/nvim/init.vim .config/nvim/init.vim
