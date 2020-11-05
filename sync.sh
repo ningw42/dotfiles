5c5900b7fe51d67e553d1220f28236d8ed91ac12
Ning Wang <unidentifiedme26@gmail.com>
2020-11-05 21:59:52 +0800

    remove 'kumiko' from remote machines

--- sync.sh
+++ sync.sh
@@ -1,7 +1,7 @@
 #! /usr/local/bin/zsh
 
 # rsync is required on remote
-remotes=(kumiko mizore nozomi)
+remotes=(mizore nozomi)
 
 for remote in ${remotes[@]}; do
 	rsync --exclude ".git" --exclude "sync.sh" --exclude "backup.sh" -r . $remote:~
